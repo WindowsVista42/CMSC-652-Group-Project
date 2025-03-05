356c828ffa9159c405e626793b384f9e4290abb4
Sean Moulton <34073738+WindowsVista42@users.noreply.github.com>
2025-03-05 12:41:24 -0500

    Handle edge case, fixup comments

--- watermarking-attack/download_datasets.py
+++ watermarking-attack/download_datasets.py
@@ -7,24 +7,28 @@ XRAY_RAW_PATH = "datasets/ct-scan/raw"
 MRI_RAW_PATH = "datasets/ct-scan/raw"
 CTSCAN_RAW_PATH = "datasets/ct-scan/raw"
 
+# Download dataset from kaggle and move it to the output folder.
 def download_dataset(kaggle_path, output_folder):
     print("Downloading dataset:", kaggle_path)
 
     path = kagglehub.dataset_download(kaggle_path)
     print("Dataset downloaded to:", path)
 
-    # move files
+    # Recreate folder structure if it doesn't exist for some reason.
+    os.makedirs(output_folder, exist_ok=True)
+
+    # Move files.
     items = glob.glob(os.path.join(path, "*"))
     for item in items:
         move_path = os.path.join(output_folder, os.path.basename(item))
 
-        # "Fresh install" of the dataset
+        # "Fresh install" of the dataset.
         if os.path.exists(move_path):
             shutil.rmtree(move_path)
 
         shutil.move(item, output_folder)
     
-    # post-cleanup folder so kaggle plays nice
+    # Post-cleanup folder so kaggle plays nice.
     shutil.rmtree(path)
 
     print("Moved files to:", output_folder)
@@ -32,7 +36,7 @@ def download_dataset(kaggle_path, output_folder):
 
 if __name__ == "__main__":
     download_dataset("sachinkumar413/cxr-2-classes", XRAY_RAW_PATH)
-    print() # makes the output a little nicer
+    print() # Nicer output.
     download_dataset("masoudnickparvar/brain-tumor-mri-dataset", MRI_RAW_PATH)
-    print() # makes the output a little nicer
+    print() # Nicer output.
     download_dataset("plameneduardo/sarscov2-ctscan-dataset", CTSCAN_RAW_PATH)
